d99ccc4c20d478fe5cdcfde39194e761436b2abe
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-07-14 21:19:49 -0300

    .

--- AppRH/src/main/java/com/AppRH/AppRH/Controller/VagaController.java
+++ AppRH/src/main/java/com/AppRH/AppRH/Controller/VagaController.java
@@ -3,7 +3,6 @@ package com.AppRH.AppRH.Controller;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
